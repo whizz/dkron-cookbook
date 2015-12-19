22aefe76083fb61d5fd2f6193951242b652acb32
Michal Táborský <michal@taborsky.cz>
2015-12-19 15:33:56 +0100

    Fixate dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email "michal@taborsky.cz"
 license          "Apache 2.0"
 description      "Installs and configures dkron server or agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
-depends 'ark'
-depends 'runit'
+depends 'ark', '~> 0.9'
+depends 'runit', '~> 1.7'
 
 supports 'redhat'
 supports 'centos'
