2e8d01f1ed389e6b99caa46690b6618c9fc243dd
Michal Táborský <michal@taborsky.cz>
2016-02-21 15:43:16 +0100

    Refactor for foodcritic test to pass

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,36 +1,37 @@
 # version to install
-default[:dkron][:version] = "0.6.4"
-default[:dkron][:download_url] = "https://github.com/victorcoder/dkron/releases/download/v0.6.4/dkron_0.6.4_linux_amd64.tar.gz"
-default[:dkron][:checksum] = "d1c10b1b69698f52a18cd17a1cdd126a8c4f384d7ba399a0373326df7db44d53"
+default['dkron']['version'] = "0.6.4"
+default['dkron']['download_url'] = "https://github.com/victorcoder/dkron/releases/download/v0.6.4/dkron_0.6.4_linux_amd64.tar.gz"
+default['dkron']['checksum'] = "d1c10b1b69698f52a18cd17a1cdd126a8c4f384d7ba399a0373326df7db44d53"
 
 # where to put it, ark will create a directory for each version and
 # create a 'current' symlink to it in this home
-default[:dkron][:install_path] = "/opt/dkron"
+default['dkron']['install_path'] = "/opt/dkron"
 
 # dkron configuration, the config variables and defaults are directly copied
 # from http://dkron.io/docs/configuration/
 #
 # most can be left as they are, but you should specify the 'join' array
 # if this is supposed to be a real world cluster, not a standalone node 
-default[:dkron][:config][:node] = node[:fqdn]
-default[:dkron][:config][:bind] = "0.0.0.0:8946"
-default[:dkron][:config][:http_addr] = ":8080"
-default[:dkron][:config][:backend] = "etcd"
-default[:dkron][:config][:backend_machine] = "127.0.0.1:2379"
-default[:dkron][:config][:tags] = {}  # tags should be key/value pairs
-default[:dkron][:config][:server] = false
-default[:dkron][:config][:keyspace] = "dkron"
-default[:dkron][:config][:encrypt] = "kPpdjphiipNSsjd4QHWbkA=="  # you should change this in your environment
-default[:dkron][:config][:mail_host] = nil
-default[:dkron][:config][:mail_port] = nil
-default[:dkron][:config][:mail_username] = nil
-default[:dkron][:config][:mail_password] = nil
-default[:dkron][:config][:mail_from] = nil
-default[:dkron][:config][:webhook_url] = nil
-default[:dkron][:config][:webhook_payload] = nil
-default[:dkron][:config][:webhook_header] = []
-default[:dkron][:config][:debug] = false
-default[:dkron][:config][:ui_dir] = nil
-default[:dkron][:config][:rpc_port] = 6868
-default[:dkron][:config][:join] = []
+default['dkron']['config']['node'] = node['fqdn']
+default['dkron']['config']['bind'] = "0.0.0.0:8946"
+default['dkron']['config']['http_addr'] = ":8080"
+default['dkron']['config']['backend'] = "etcd"
+default['dkron']['config']['backend_machine'] = "127.0.0.1:2379"
+default['dkron']['config']['tags'] = {}  # tags should be key/value pairs
+default['dkron']['config']['server'] = false
+default['dkron']['config']['keyspace'] = "dkron"
+# you should change this in your environment
+default['dkron']['config']['encrypt'] = "kPpdjphiipNSsjd4QHWbkA=="
+default['dkron']['config']['mail_host'] = nil
+default['dkron']['config']['mail_port'] = nil
+default['dkron']['config']['mail_username'] = nil
+default['dkron']['config']['mail_password'] = nil
+default['dkron']['config']['mail_from'] = nil
+default['dkron']['config']['webhook_url'] = nil
+default['dkron']['config']['webhook_payload'] = nil
+default['dkron']['config']['webhook_header'] = []
+default['dkron']['config']['debug'] = false
+default['dkron']['config']['ui_dir'] = nil
+default['dkron']['config']['rpc_port'] = 6868
+default['dkron']['config']['join'] = []
     
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,26 +2,26 @@ include_recipe 'runit::default'
 
 # download and install 
 ark "dkron" do
-  url node[:dkron][:download_url]
-  checksum node[:dkron][:checksum]
-  version node[:dkron][:version]
-  prefix_root node[:dkron][:install_path]
-  home_dir "#{node[:dkron][:install_path]}/current"
+  url node['dkron']['download_url']
+  checksum node['dkron']['checksum']
+  version node['dkron']['version']
+  prefix_root node['dkron']['install_path']
+  home_dir "#{node['dkron']['install_path']}/current"
   action :install
 end
 
 # create the config json
-template "#{node[:dkron][:install_path]}/current/config/dkron.json" do
+template "#{node['dkron']['install_path]}/current/config/dkron.json" do
   source "config.json.erb"
   mode "0664"
-  variables :config => node[:dkron][:config]
+  variables :config => node['dkron']['config']
   notifies :restart, "runit_service[dkron]"
 end
 
 # create a service thru runit
 runit_service 'dkron' do
   options(
-    home: "#{node[:dkron][:install_path]}/current"
+    home: "#{node['dkron']['install_path']}/current"
   )
   action :enable
 end
