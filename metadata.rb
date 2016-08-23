12215a00dfa9061dfba4afa57b5157fa5cf8e4bf
Michal Táborský <michal@taborsky.cz>
2016-08-23 14:20:07 +0200

    Move the repo

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license 'Apache 2.0'
 description 'Installs and configures dkron server or agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.1'
-source_url 'https://gitlab.com/nrh/dkron-cookbook'
-issues_url 'https://gitlab.com/nrh/dkron-cookbook/issues'
+source_url 'https://gitlab.com/mallgroup/dkron-cookbook'
+issues_url 'https://gitlab.com/mallgroup/dkron-cookbook/issues'
 
 depends 'ark', '~> 1.0'
 depends 'runit', '~> 1.7'
